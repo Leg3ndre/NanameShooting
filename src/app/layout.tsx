c394524bd10211849deba867fa84ae099ca7fb10
Leg3ndre <leg3ndre@gmail.com>
2025-04-30 02:32:01 +0900

    three.jsのロード確認

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ja">
+      <body>
         {children}
       </body>
     </html>
