7c252d50acdc618dff5bb2bb5949379ea648eaec
Leg3ndre <leg3ndre@gmail.com>
2025-04-30 03:03:13 +0900

    HTML素案

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import styles from './page.module.css';
+// import styles from './page.module.css';
 import * as THREE from 'three';
 
-export default function Home() {
-  console.log('DEBUG:',THREE);
+const Home = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-      </main>
-    </div>
+    <>
+      <h1>Naname Shooting</h1>
+      <canvas id="game" width="640" height="480"></canvas>
+    </>
   );
 }
+
+export default Home;
