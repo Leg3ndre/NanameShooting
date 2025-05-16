8d1cd0d2736fcd4ab17829f756c16c6d42f49dc2
Leg3ndre <leg3ndre@gmail.com>
2025-05-17 02:05:28 +0900

    build export

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
+  output: 'export',
+  assetPrefix: process.env.DEPLOY_BASE_PATH,
+  basePath: process.env.DEPLOY_BASE_PATH,
 };
 
 export default nextConfig;
